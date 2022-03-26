cd5e9cf17919a67e5d98a2108ea6a005723df2b9
Kevin <kevins.shi2001@gmail.com>
2022-03-25 19:35:56 -0700

    added some buttons

---
+++ pages/login.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from "next/link";
+
+export default function Testing() {
+  
+
+  return (
+    <div>
+      <Link href="/" passHref>
+          <button >Sign in</button>
+      </Link>
+      <div>hello</div>
+    </div>
+  )
+}
---
+++ pages/test2.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function testing2() {
+  
+
+  return (
+    
+    <div className={styles.container}>
+      <p>HELLO 2</p>
+    </div>
+  )
+}
