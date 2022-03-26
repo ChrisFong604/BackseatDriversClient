c47ed4d8055e628f9a76dd2dddc45ffb97e89fea
Kevin <kevins.shi2001@gmail.com>
2022-03-26 11:39:17 -0700

    added navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
 
+
+import NavigationBar from './navbar';
 import Testing from './login'
 
 export default function Home() {
@@ -12,7 +14,7 @@ export default function Home() {
   return (
     
     <div className={styles.container}>
-      
+      <NavigationBar/>
       <Testing/>
       <Head>
         <title>Create Next App</title>
--- pages/postings.jsx
+++ pages/postings.jsx
@@ -18,6 +18,17 @@ function AllPosting({dummydata}){
                 <div key = {xd.date_of_ride}>
                     <p>{xd.date_of_ride}</p>
                     <p>{xd.number_of_seats}</p>
+                    <iframe
+                        width="450"
+                        height="250"
+                        
+                        
+                        src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyDXYIKlauR9teuVU4RHWACY6T1x_fPbZFY
+                        &origin= ${xd.departure_location}
+                        &destination= ${xd.school_location}`}
+                        
+                    >
+                    </iframe>
                     <br></br>
                 </div>
             )    
@@ -36,17 +47,7 @@ export default function Postings() {
         <div className={styles.container}>
         <p>THIS PAGE WILL BE TO VIEW ALL LISTINGS</p>
         <AllPosting dummydata = {dummy}/>
-        <iframe
-            width="450"
-            height="250"
-            
-            
-            src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyDXYIKlauR9teuVU4RHWACY6T1x_fPbZFY
-            &origin= ${origin}
-            &destination= ${destination}`}
-            
-        >
-        </iframe>
+        
         </div>
   )
 }
