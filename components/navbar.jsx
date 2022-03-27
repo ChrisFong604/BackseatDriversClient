f715e8a85199ffe0df524cf1cb06039473d49a58
ChrisFong604 <ccffoonngg@gmail.com>
2022-03-27 04:37:53 -0700

    Added submit for ride form

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,29 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Container, Nav} from 'react-bootstrap/'
-
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav } from "react-bootstrap/";
 
 export default function NavigationBar() {
-  
-
-  return (
-    
-    <>
-        <Navbar bg="dark" variant="dark">
-            <Container>
-            <Navbar.Brand href="/"> <h2 style = {{marginLeft:-185}}>Backseat Drivers</h2></Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link href="/drivers" style = {{marginLeft: 100}}><h4>Drivers</h4></Nav.Link>
-            <Nav.Link href="postings" style = {{marginLeft: 40}}><h4>Postings</h4></Nav.Link>
-            <Nav.Link href="/riderProfile" style = {{marginLeft: 900}}><h4>Profile</h4></Nav.Link>
-            </Nav>
-            </Container>
-        </Navbar>
-        
-    </>
-  )
+	return (
+		<>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="/">
+						{" "}
+						<h2 style={{ marginLeft: -185 }}>Backseat Drivers</h2>
+					</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link
+							href="/create-ride"
+							style={{ paddingLeft: 100, paddingRight: 100 }}
+						>
+							<h4>Create Ride</h4>
+						</Nav.Link>
+						<Nav.Link href="postings" style={{ marginLeft: 40 }}>
+							<h4>Postings</h4>
+						</Nav.Link>
+						<Nav.Link href="/riderProfile" style={{ marginLeft: 900 }}>
+							<h4>Profile</h4>
+						</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+		</>
+	);
 }
